a214f1336d60d34c31075665663eeccae7d9a7b3
jjpjp6533 <104783053+arasjose@users.noreply.github.com>
2022-08-10 23:15:07 +0800

    sdjha

--- detect.js
+++ detect.js
@@ -1,8 +1,12 @@
- function isFacebookApp() {
+function isFacebookApp() {
         var ua = navigator.userAgent || navigator.vendor || window.opera;
         return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
     } 
     if(isFacebookApp()){
-         // your action here if user using Facebook in-app browser
-        alert('Are you using Facebook Browser? To get better experience, try press [...] and Open the blog in Chrome or Safari. Thank you!');
+         // fade in notice bar when detect Facebook in-app browser
+          $('#susu-fb-inapp').fadeIn();
+         // fade out notice bar when user press OK
+          $('#susu-fb-inapp').on('click', function(event) {
+              $('#susu-fb-inapp').fadeOut();   
+          });
     }
