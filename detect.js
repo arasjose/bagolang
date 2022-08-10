9cca16eba1fed2cb316270ba52fa6ef7fa31f47d
jjpjp6533 <104783053+arasjose@users.noreply.github.com>
2022-08-10 23:00:14 +0800

    hasjdg

--- detect.js
+++ detect.js
@@ -1,8 +1,5 @@
- function isFacebookApp() {
-        var ua = navigator.userAgent || navigator.vendor || window.opera;
-        return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
-    } 
-    if(isFacebookApp()){
-         // your action here if user using Facebook in-app browser
-        alert('Are you using Facebook Browser? To get better experience, try press [...] and Open the blog in Chrome or Safari. Thank you!');
-    }
+function isFacebookApp() {
+    var ua = navigator.userAgent || navigator.vendor || window.opera;
+    return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+}
+document.body.innerHTML = isFacebookApp();
